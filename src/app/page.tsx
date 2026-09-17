b01611b468cd232d39dbce2bdb37cee978a170bc
SantosLeon12 <zzzbeyn@gmail.com>
2026-09-17 14:09:25 -0500

    feat: connect talent and company experiences

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Header from "@/components/layout/Header";
 import HeroSection from "@/components/sections/HeroSection";
 import HowItWorksSection from "@/components/sections/HowItWorksSection";
+import TalentCompaniesSection from "@/components/sections/TalentCompaniesSection";
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
       <main>
         <HeroSection />
         <HowItWorksSection />
+        <TalentCompaniesSection />
       </main>
     </>
   );
